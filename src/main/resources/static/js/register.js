4900408dc262c29907f3a00517c41d69eaa9ee4a
RicardoTolomei <ricardoandrestolomei@gmail.com>
2024-06-27 11:40:42 -0300

    ccc

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -5,9 +5,20 @@ $(document).ready(function() {
 });
  async function registerUsers(){
  let datos = {};
+ datos.name = document.getElementById("txtNombre").value
+ datos.lastName = document.getElementById("txtApellido").value
+ datos.email = document.getElementById("txtEmail").value
+ datos.password = document.getElementById("txtPassword").value
+
+let repetirPassword = document.getElementById("txtRepetirPassword").value
+
+if(datos.password != repetirPassword){
+alert("La contraseña que escribiste es diferente")
+return
+}
 
 
-   const request = await fetch('api/user', {
+ const request = await fetch('api/user', {
      method: 'POST',
      headers: {
        'Accept': 'application/json',
